eae4fc17e75b54c079ab6c79b92724cab25c401f
alvaromendoooo <alvaromendo2004@gmail.com>
2026-09-08 10:59:15 +0200

    done

--- main.rs
+++ main.rs
@@ -1,8 +1,10 @@
 use std::io::{self, BufRead};
 use std::rc::{self, Rc};
+use std::cell::RefCell;
 
 fn main() {
-    let stdin = io::stdin();
+    // Test 01
+    /*let stdin = io::stdin();
     let mut line = String::new();
     stdin.lock().read_line(&mut line).unwrap();
     let nums: Vec<i32> = line.split_whitespace().map(|s| s.parse().unwrap()).collect();
@@ -12,5 +14,15 @@ fn main() {
     let _handle2 = Rc::clone(&rc_nums);
 
     println!("count: {}", Rc::strong_count(&rc_nums));
-    println!("sum: {}", nums.iter().sum::<i32>());
+    println!("sum: {}", nums.iter().sum::<i32>());*/
+
+    // Test 02
+    let counter = Rc::new(RefCell::new(0));
+
+    for _ in 0..3 {
+        let handle = Rc::clone(&counter);
+        *handle.borrow_mut() += 1;
+    }
+
+    println!("{}", counter.borrow());
 }
